8f6849373cd3f346c447d91b179ddca81d3023d1
Son Seung Min <sonsmmm@naver.com>
2025-03-12 17:02:23 +0900

    JS 커밋

---
+++ yalko/main.js
@@ -0,0 +1,18 @@
+import Big from './node_modules/big.js/big.mjs';
+
+console.log(Big);
+
+console.log(
+  0.1 + 0.2,
+  new Big(0.1).plus(0.2).toNumber()
+);
+
+console.log(
+  0.2 * 0.7,
+  new Big(0.2).times(0.7).toNumber(),
+);
+
+console.log(
+  0.9 - 0.6,
+  new Big(0.9).minus(0.6).toNumber()
+);
