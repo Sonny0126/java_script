2917fe121c02f425ad0282b474f301edbfbdc674
Son Seung Min <sonsmmm@naver.com>
2025-06-09 21:52:36 +0900

    final(before) animal

--- animal/src/App.js
+++ animal/src/App.js
@@ -3,43 +3,42 @@ import Content from './components/Content.js';
 import { request } from './components/api.js';
 
 export default function App($app) {
-    this.state ={
-        currentTab: 'all',
-        photos:[],
-    };
-    const tabBar= new TabBar({
-        $app,
-        initialState: '',
-        onClick: async (name)=> {
-            this.setState({
-                ...this.state,
-                currentTab: name,
-                photos: await request(name)
-            });
-        },
+  this.state = {
+    currentTab: 'all',
+    photos: [],
+  };
 
-    });
-    const content = new Content();
+  const tabBar = new TabBar({
+    $app,
+    initialState: 'all',
+    onClick: async (name) => {
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos: await request(name),
+      });
+    },
+  });
 
-    this.setState = (newState)=>{
-        this.state=newState;
-        tabBar.this.setState(this.state.currentTab);
-        content.setState(this.state.photos);
-    };
+  const content = new Content({ $app });
 
-    //초기상태 설정
-    const init = async ()=>{
-        try{
-            const initialPhotos = await request();
-            this.setState({
-                ...this.state,
-                photos: initialPhotos,
-            });
-        }catch(err){
-            console.log(err);
-        }
+  this.setState = (newState) => {
+    this.state = newState;
+    tabBar.setState(this.state.currentTab); // ❗ 수정: tabBar.this ❌
+    content.setState(this.state.photos);
+  };
+
+  const init = async () => {
+    try {
+      const initialPhotos = await request();
+      this.setState({
+        ...this.state,
+        photos: initialPhotos,
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
+
+  init(); // 초기화 함수 호출 추가도 잊지 마세요!
 }
-
-
-
--- animal/src/components/TabBar.js
+++ animal/src/components/TabBar.js
@@ -1,24 +1,40 @@
-export default function TabBar({$app,initialSate, onCLick}) {
-    this.state={};
-    this.onCLick= onclick;
+export default function TabBar({ $app, initialSate, onClick }) {
+  this.state = initialSate;  // 초기 상태 설정
+  this.onClick = onClick;    // ✅ 올바르게 저장
 
-    this.$target=document.createElement('div');
-    this.$target.className='tab-bar';
-    $app.appendChild(this.$target);
+  this.$target = document.createElement('div');
+  this.$target.className = 'tab-bar';
+  $app.appendChild(this.$target);
 
-    this.template=()=>{
-        let temp=`<div id="all">전체</div><div id = "penguin">펭귄</div>
-        <div id = "koala">코알라</div><div id ="panda">판다</div>`;
+  this.template = () => {
+    return `
+      <div id="all">전체</div>
+      <div id="penguin">펭귄</div>
+      <div id="koala">코알라</div>
+      <div id="panda">판다</div>
+    `;
+  };
 
-        return temp;
-    };
+  this.render = () => {
+    this.$target.innerHTML = this.template();
 
-    this.render = () =>{
-        this.$target.innerHTML=this.template();
-    };
+    // 현재 탭 강조
+    const $currentTab = document.getElementById(this.state);
+    if ($currentTab) $currentTab.classList.add('clicked');
 
-    this.setState = (newState)=>{
-        this.state=newState;
-        this.render();
-    }
+    // 클릭 이벤트 바인딩
+    const $tabItems = this.$target.querySelectorAll('div');
+    $tabItems.forEach((elm) => {
+      elm.addEventListener('click', () => {
+        this.onClick(elm.id);        // ✅ this.onClick 사용
+      });
+    });
+  };
+
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+
+  this.render();  // 초기 렌더링
 }
